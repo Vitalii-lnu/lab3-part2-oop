075a6661682569b887a76d5804751aa792e80c9d
Vitalii <Vitalii.Popovych@lnu.edu.ua>
2026-05-12 15:29:30 +0300

    Changed functionality

--- NewtonSolver.h
+++ NewtonSolver.h
@@ -6,16 +6,16 @@
 
 class NewtonSolver {
 private:
-    double epsilon; // Точність
-    double dx;      // Крок для обчислення похідної
+    double epsilon; 
+    double dx;      
 
-    // Метод для чисельного обчислення похідної: f'(x) ≈ [f(x + dx) - f(x)] / dx
+    
     double derive(std::function<double(double)> f, double x) const;
 
 public:
     NewtonSolver(double eps = 1e-7, double deltaX = 1e-8);
 
-    // Основний метод пошуку кореня
+    
     double solve(std::function<double(double)> f, double x0, int max_iterations = 1000) const;
 };
 
--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main() {
     setlocale(0, ".1251");
 
 
-    // Визначаємо функцію f(x) = x^2 - 2
+    
     auto myFunction = [](double x) {
         return std::exp(x) - std::exp(-x) - 2;
     };
 
-    NewtonSolver solver(0.01); // Створюємо об'єкт з заданою точністю
-    double initialGuess = 1.0;  // Початкове наближення
+    NewtonSolver solver(0.01); 
+    double initialGuess = 1.0;
 
     
     double root = solver.solve(myFunction, initialGuess);
